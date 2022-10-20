a123b6fc732a544fa3a757f992f83007fa33f5c0
ozheng2 <ozheng@stu.socsd.org>
2022-10-20 14:15:55 -0400

    Update randomGreeting.js

--- randomGreeting.js
+++ randomGreeting.js
@@ -1,10 +1,11 @@
-let theList = ["hello", "there", "john", "how", "are", "you", "doing"]
+let theList = ["Hello", "Hola", "Bonjour", "Guten Tag", "Salve", "Zdravstvuyte", "你好", "こんにちは", "Olá", "안녕하세요", "Asalaam alaikum", "Goddag", "Shikamoo", "Goedendag", "Yassas", "Dzień dobry"
+, "Selamat siang", "Namaste", "Namaskar", "Merhaba", "Shalom", "God dag", "Hej"]
 
 
 function randomWordPicker(aList){
     let theListLength = theList.length / 10
     let theAnswer = Math.floor( ( Math.random( ) * 10 ) * theListLength  ) 
-    return aList[theAnswer]
+    return aList[theAnswer] + ", I'm Oscar"
 }
 
 let result = randomWordPicker(theList)
