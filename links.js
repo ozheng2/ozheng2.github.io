b0382dd7071bca7ba9d7d9710dd42047c4771efe
ozheng2 <ozheng@stu.socsd.org>
2023-12-08 09:41:58 -0500

    jen

---
+++ links.js
@@ -0,0 +1,9 @@
+var pong = "https://github.com/ozheng2/AP-CS-Final-Project/tree/main/Pong%20Project";
+var wealthDistribution = "https://github.com/ozheng2/AP-CS-Final-Project/tree/main/Wealth%20Distribution%20Lab";
+
+
+var linkList = [pong, wealthDistribution];
+
+function getLink(index){
+    return linkList[index];
+}
